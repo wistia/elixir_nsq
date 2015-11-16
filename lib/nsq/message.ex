19c65fef007f0c7fadd8716ac5d3ca2361d97dcc
Max Schnur <mschnur@wistia.com>
2015-11-16 16:06:05 -0500

    Simplify some stuff in NSQ.Message.

--- lib/nsq/message.ex
+++ lib/nsq/message.ex
@@ -1,12 +1,13 @@
 defmodule NSQ.Message do
   import NSQ.Protocol
   use GenServer
+  require Logger
 
 
   defstruct [:id, :timestamp, :attempts, :data]
 
 
-  @initial_state %{message: nil, socket: nil, handler: nil}
+  @initial_state %{message: nil, socket: nil, handler: nil, handler_pid: nil}
 
 
   def from_data(data) do
@@ -37,13 +38,10 @@ defmodule NSQ.Message do
   We expect the consumer connection to call NSQ.Message.process(pid, :process),
   which will both create the process and kick off the async job.
   """
-  def handle_call(cmd, from, state) do
-    case cmd do
-      :process ->
-        spawn_link fn -> do_handle_process(state, from) end
-      _else ->
-        IO.inspect "Unhandled command #{inspect cmd} from #{from}"
-    end
+  def handle_call(:process, from, state) do
+    handler_pid = spawn_link(fn -> do_handle_process(state, from) end)
+    Logger.error "kick off"
+    state = %{state | handler_pid: handler_pid}
     {:noreply, state}
   end
 
@@ -51,42 +49,40 @@ defmodule NSQ.Message do
   defp do_handle_process(%{message: message, handler: handler} = state, from) do
     case run_handler(handler, message) do
       {:ok} ->
-        finish(state.socket, message)
+        fin(state.socket, message)
         end_processing(message, from)
       {:req, delay} ->
-        requeue(state.socket, message)
+        req(state.socket, message)
         end_processing(message, from)
-      anything ->
-        IO.puts "Unmatched handler.message return value #{inspect anything}"
     end
   end
 
 
   @doc """
   Handler can be either an anonymous function or a module that implements the
-  `message` function.
+  `handle_message\2` function.
   """
   defp run_handler(handler, message) do
     if is_function(handler) do
       handler.(message.data, message)
     else
-      handler.message(message.data, message)
+      handler.handle_message(message.data, message)
     end
   end
 
 
-  def finish(socket, message) do
+  def fin(socket, message) do
     :gen_tcp.send(socket, encode({:fin, message.id}))
   end
 
 
-  def requeue(socket, message, delay \\ 1000) do
+  def req(socket, message, delay \\ 1000) do
     :gen_tcp.send(socket, encode({:req, message.id, delay}))
   end
 
 
   defp end_processing(message, {pid, ref} = from) do
     GenServer.call(pid, {:done, message})
-    Process.exit(self, :normal)
+    GenServer.stop(self)
   end
 end
