26e33d9de2f7033d156cfe139232619d9f69ac02
Max Schnur <mschnur@wistia.com>
2016-01-13 21:56:27 -0500

    Include :socket in applications for exrm.

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule ElixirNsq.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpotion, :poison]]
+    [applications: [:logger, :httpotion, :poison, :socket]]
   end
 
   # Dependencies can be Hex packages:
