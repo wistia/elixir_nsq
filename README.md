0ba55058a1baaa2bead59072e7806fefcd90ca76
Max Schnur <mschnur@wistia.com>
2016-01-22 16:05:42 -0500

    Version bump to 1.0.3.

--- README.md
+++ README.md
@@ -36,7 +36,7 @@ NSQ.Producer.close(producer)
 
 ```elixir
 defp deps do
-  [{:elixir_nsq, "~> 1.0.2"}]
+  [{:elixir_nsq, "~> 1.0.3"}]
 end
 
 defp applications do
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ElixirNsq.Mixfile do
 
   def project do
     [app: :elixir_nsq,
-     version: "1.0.2",
+     version: "1.0.3",
      elixir: "~> 1.1",
      description: description,
      package: package,
