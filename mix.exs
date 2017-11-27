410ce9a3b32d302ced9b4f60c77a364a8d27eb22
Max Schnur <mschnur@wistia.com>
2017-11-26 20:55:11 -0500

    Version bump.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ElixirNsq.Mixfile do
 
   def project do
     [app: :elixir_nsq,
-     version: "1.0.3",
+     version: "1.0.4",
      elixir: "~> 1.1",
      description: description(),
      package: package(),
