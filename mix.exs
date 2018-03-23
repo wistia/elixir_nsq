3f6514f8891b82e914d473d47cfeec5e525b889a
Max Schnur <mschnur@wistia.com>
2018-03-23 17:08:49 -0400

    Update package version.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ElixirNsq.Mixfile do
 
   def project do
     [app: :elixir_nsq,
-     version: "1.0.4",
+     version: "1.1.0",
      elixir: "~> 1.1",
      description: description(),
      package: package(),
