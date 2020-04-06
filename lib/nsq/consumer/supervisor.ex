9ca912b1bce6ec5a4e184686b70b96533b6c2735
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2020-04-06 19:32:25 +0700

    lib/nsq/consumer: use map child_spec to spawn Task
    Fixes #1

--- lib/nsq/consumer/supervisor.ex
+++ lib/nsq/consumer/supervisor.ex
@@ -8,11 +8,20 @@ defmodule NSQ.Consumer.Supervisor do
   @impl true
   def init({topic, channel, config}) do
     consumer_name = String.to_atom("nsq_consumer_#{UUID.uuid4(:hex)}")
+    discovery_loop_id = String.to_atom("#{consumer_name}_discovery_loop")
+    rdy_loop_id = String.to_atom("#{consumer_name}_rdy_loop")
 
     children = [
       {NSQ.Consumer, {topic, channel, consumer_name, config}},
-      {NSQ.Consumer.ConnectionsTask, consumer_name},
-      {NSQ.Consumer.RDYTask, consumer_name}
+      %{
+        :id => discovery_loop_id,
+        :start =>
+          {Task, :start_link, [NSQ.Consumer.Connections, :discovery_loop, [consumer_name]]}
+      },
+      %{
+        :id => rdy_loop_id,
+        :start => {Task, :start_link, [NSQ.Consumer.RDY, :redistribute_loop, [consumer_name]]}
+      }
     ]
 
     Supervisor.init(children, strategy: :rest_for_one)
