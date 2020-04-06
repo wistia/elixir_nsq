832cca29675e348d1f06d6aac2321f425356a95c
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2020-04-06 22:19:13 +0700

    lib/nsq/message: migrate Supervisor.Spec to Supervisor
    Fixes #8

--- lib/nsq/message/supervisor.ex
+++ lib/nsq/message/supervisor.ex
@@ -15,15 +15,18 @@ defmodule NSQ.Message.Supervisor do
     Supervisor.start_link(__MODULE__, :ok, opts)
   end
 
-  def start_child(msg_sup_pid, message, opts \\ []) do
+  def start_child(msg_sup_pid, message) do
     # If a message fails, NSQ will handle requeueing.
-    id = message.id <> "-" <> UUID.uuid4(:hex)
-    opts = [restart: :temporary, id: id] ++ opts
-    child = worker(NSQ.Message, [message], opts)
-    Supervisor.start_child(msg_sup_pid, child)
+    id = "#{message.id}-#{UUID.uuid4(:hex)}"
+
+    Supervisor.start_child(msg_sup_pid, %{
+      :id => id,
+      :start => {NSQ.Message, :start_link, [message]},
+      :restart => :temporary
+    })
   end
 
   def init(:ok) do
-    supervise([], strategy: :one_for_one)
+    Supervisor.init([], strategy: :one_for_one)
   end
 end
