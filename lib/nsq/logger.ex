e119db2435d4fa2e3df63aeb128a18be6b25d6df
benonymus <bence1204@gmail.com>
2023-12-13 12:11:39 +0700

    fix doc warnings

--- lib/nsq/logger.ex
+++ lib/nsq/logger.ex
@@ -5,17 +5,16 @@ defmodule NSQ.Logger do
   defdelegate info(message), to: Logger
   defdelegate error(message), to: Logger
 
-  def warn(message) do
-    case Version.compare(System.version(), "1.11.0") do
-      :lt -> Logger.warn(message)
-      _ -> Logger.warning(message)
-    end
+  case Version.compare(System.version(), "1.11.0") do
+    :lt -> defdelegate warn(message), to: Logger
+    _ -> defdelegate warn(message), to: Logger, as: :warning
   end
 
   def configure(opts) do
     case {opts, Version.compare(System.version(), "1.11.0")} do
       {[level: :warn], :gt} ->
-        Logger.configure(Keyword.merge(opts, [level: :warning]))
+        Logger.configure(Keyword.merge(opts, level: :warning))
+
       _ ->
         Logger.configure(opts)
     end
